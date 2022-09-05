686645cc7652f5adaabaf5cde6cbc0b53564f49a
dreamworldsn <107345428+gardenwow@users.noreply.github.com>
2022-09-06 00:03:39 +0400

    обьекты ч2

--- src/Author.java
+++ src/Author.java
@@ -1,6 +1,13 @@
+import java.util.Objects;
+
 public class Author {
     private String nameAuthor;
     private String surName;
+    private String fullName;
+
+    public String getFullName() {
+        return this.nameAuthor + " " + this.surName;
+    }
 
     public Author(String nameAuthor, String surName){
         this.nameAuthor = nameAuthor;
@@ -15,5 +22,21 @@ public class Author {
 
     public String getSurName() {
         return surName;
+    }
+    public String toString(){
+        return "name: " + this.nameAuthor + " surname: " + this.surName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(nameAuthor, author.nameAuthor) && Objects.equals(surName, author.surName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameAuthor, surName);
     }
 }
